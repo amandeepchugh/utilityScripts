64d2d5298a6b379c48c53e0eab38748119031511
Amadeep Chugh <chugha@vmware.com>
2016-10-16 00:16:06 +0530

    Adding TODO

--- pushBranchToRemote.sh
+++ pushBranchToRemote.sh
@@ -5,6 +5,8 @@ currentBranch=`git rev-parse --abbrev-ref HEAD`
 userName=`whoami`
 path='topic/'$userName
 
+# put check if there's un-committed code
+
 # TODO: no need to delete and create if branches are identical: check the hash of the already existent remote branch matches with the local branch
 git push $remoteRepository :$path/$currentBranch  #delete existing branch if any
 git push $remoteRepository HEAD:$path/$currentBranch #create a new branch
